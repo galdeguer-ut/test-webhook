74d1afd6d4e764736bdffbe67cae06fb19c7a587
Guillem Aldeguer Lima <galdeguer@userzoom.com>
2020-03-09 12:18:10 +0100

    fix: fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { verifyPostData } = require('../middlewares');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/github', verifyPostData, (req, res) => {
   const githubEvent = req.body;
